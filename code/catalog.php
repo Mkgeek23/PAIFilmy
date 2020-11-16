d48baf59417700a6451d45e3343b8824f41d4cda
Maciej Olech <222528@edu.p.lodz.pl>
2020-11-16 23:15:40 +0100

    Nowe filmy, poprawki w wyswietlaniu szczegolow filmu

--- code/catalog.php
+++ code/catalog.php
@@ -4,7 +4,31 @@
 		<div class="catItems slick">
 			<?php
 		
-				$sql = "SELECT * FROM filmy ORDER BY id DESC LIMIT 25";
+				$sql = "SELECT * FROM filmy ORDER BY id DESC LIMIT 24";
+			    $result = $conn->query($sql);
+			    while($row = $result->fetch_assoc()){
+			    	echo '
+			    		<div class="catItem">
+							<div class="simplePoster">
+								<a href="index.php?a=film&fid='.$row['id'].'" class="itemPosterLink"><img src="img/movies/'.$row['image'].'"></a>
+							</div>
+							<div class="cena">'.number_format((float)$row['cenaZakupu'], 2, '.', '').' PLN</div>
+							<a href="index.php?a=film&fid='.$row['id'].'" class="itemLink"><h3>'.$row['tytul'].'</h3></a>
+						</div>
+			    		';
+			    }
+			?>
+
+		</div>
+	</div>
+</div>
+<div class="itemsContainer container-background">
+	<div class="container">
+		<h2 class="itemsContainerTitle">Najtańsze filmy</h2>
+		<div class="catItems slick">
+			<?php
+		
+				$sql = "SELECT * FROM filmy ORDER BY cenaZakupu ASC LIMIT 24";
 			    $result = $conn->query($sql);
 			    while($row = $result->fetch_assoc()){
 			    	echo '
